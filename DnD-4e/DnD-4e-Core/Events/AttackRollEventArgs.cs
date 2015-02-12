c47686caafd31d4aa9111ad3ca8b66f7bb08ed6f
einsteinsci <einsteinsci@gmail.com>
2015-02-12 06:10:10 -0800

    Quick Commit

---
+++ DnD-4e/DnD-4e-Core/Events/AttackRollEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DnD_4e.Entities;
+
+namespace DnD_4e.Events
+{
+	public class AttackRollEventArgs : PlayerEventArgs
+	{
+		public List<Entity> Targeted
+		{ get; private set; }
+	}
+}
