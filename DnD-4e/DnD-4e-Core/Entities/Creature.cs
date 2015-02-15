bbec7538e8ba5c96ae8ac442073cacbef9057665
einsteinsci <einsteinsci@gmail.com>
2015-02-14 17:40:47 -0800

    Quick Commit

---
+++ DnD-4e/DnD-4e-Core/Entities/Creature.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DnD_4e.Build.Powers;
+using DnD_4e.Core;
+using DnD_4e.Core.Modifiers;
+using DnD_4e.Core.Skills;
+using DnD_4e.Lore;
+using DnD_4e.Mechanics;
+
+namespace DnD_4e.Entities
+{
+    public abstract partial class Creature
+	{
+		public virtual string Name
+		{ get; set; }
+
+		public int Level
+		{ get; private set; }
+		public Tier Tier
+		{
+			get
+			{
+				return (Tier)((Level - 1) / 10);
+			}
+		}
+
+		public AbilityModifierSet Abilities
+		{ get; set; }
+
+		public IntModifier ArmorClass
+		{ get; private set; }
+
+		public IntModifier Fortitude
+		{ get; private set; }
+		public IntModifier Reflex
+		{ get; private set; }
+		public IntModifier Will
+		{ get; private set; }
+
+		public SkillSet Skills
+		{ get; private set; }
+
+		public IntModifier Speed
+		{ get; private set; }
+
+		public IntModifier Initiative
+		{ get; private set; }
+
+		public IntModifier PassivePerception
+		{ get; private set; }
+		public IntModifier PassiveInsight
+		{ get; private set; }
+
+		public IntModifier MaxHitPoints
+		{ get; private set; }
+		public int BloodiedValue
+		{
+			get
+			{
+				return MaxHitPoints.Result / 2;
+			}
+		}
+		public int HealingSurgeValue
+		{
+			get
+			{
+				return BloodiedValue / 2;
+			}
+		}
+
+		public IntModifier MaxHealingSurges
+		{ get; private set; }
+
+		public Dictionary<string, Resistance> Resistances // yep, there are temp resistances
+		{ get; private set; }
+
+		public Dictionary<string, Resistance> Vulnerabilities // pretty much all of these will be temp
+		{ get; private set; }
+
+		public Dictionary<string, IStatusEffect> Immunities	// of course temp immunities too
+		{ get; private set; }
+
+		public virtual CreatureStatus Status
+		{ get; private set; }
+
+		public List<PowerBase> Powers
+		{ get; private set; }
+
+		public List<Creature> Allies
+		{ get; private set; }
+
+		public List<Languages> Languages
+		{ get; private set; }
+
+		public Creature()
+		{
+			Abilities = new AbilityModifierSet(new AbilitySet());
+
+			ArmorClass = new IntModifier(0);
+			Fortitude = new IntModifier(0);
+			Reflex = new IntModifier(0);
+			Will = new IntModifier(0);
+
+			Skills = new SkillSet(Abilities);
+
+			Speed = new IntModifier(0);
+			Initiative = new IntModifier(0);
+			PassivePerception = new IntModifier(Skills[SkillType.Perception].Result);
+			PassiveInsight = new IntModifier(Skills[SkillType.Insight].Result);
+
+			MaxHitPoints = new IntModifier(0);
+			MaxHealingSurges = new IntModifier(0);
+
+			Resistances = new Dictionary<string, Resistance>();
+			Vulnerabilities = new Dictionary<string, Resistance>();
+			Immunities = new Dictionary<string, IStatusEffect>();
+		}
+	}
+}
---
+++ DnD-4e/DnD-4e-Core/Entities/CreatureStatus.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DnD_4e.Events;
+using DnD_4e.Mechanics;
+using DnD_4e.Powers;
+
+namespace DnD_4e.Entities
+{
+	public partial class Creature
+	{
+		public class CreatureStatus
+		{
+			public Creature Creature
+			{ get; private set; }
+
+			public List<Creature> Enemies
+			{ get; private set; }
+
+			public int UsedHealingSurges
+			{ get; set; }
+
+			public int HitPoints
+			{ get; set; }
+			public int TemporaryHitPoints
+			{ get; set; }
+			public bool Bloodied
+			{
+				get
+				{
+					return HitPoints < Creature.BloodiedValue;
+				}
+			}
+			public bool Dying
+			{
+				get
+				{
+					return HitPoints <= 0 && !KO;
+				}
+			}
+
+			/// <summary>
+			/// Set to true when someone chooses to knock the creature out but not put them in the "dying" state.
+			/// </summary>
+			public bool KO
+			{ get; set; }
+
+			public int ActionPointsLeft
+			{ get; set; }
+
+			public List<IStatusEffect> StatusEffects
+			{ get; set; }
+
+			public CreatureStatus(Creature creature)
+			{
+				Creature = creature;
+			}
+
+			public void ApplyDamage(Damage dmg, Creature attacker)
+			{
+				ApplyDamage(dmg, 1.0f, attacker);
+			}
+			public void ApplyDamage(Damage dmg, float mult, Creature attacker)
+			{
+				ApplyDamage(dmg, mult, attacker, false);
+			}
+			public virtual void ApplyDamage(Damage damage, float damageMultiplier, Creature attacker, bool ko)
+			{
+				int hpLoss = damage.Amount;
+
+				#region RESISTANCE
+				List<DmgType> resistanceTypes = new List<DmgType>();
+				foreach (Resistance r in Creature.Resistances.Values)
+				{
+					resistanceTypes.Add(r.Type);
+				}
+
+				bool hasResistanceToAll = true;
+				foreach (DmgType dt in damage.Types)
+				{
+					if (!resistanceTypes.Contains(dt))
+					{
+						hasResistanceToAll = false;
+						break;
+					}
+				}
+				if (hasResistanceToAll)
+				{
+					Resistance? weakest = null;
+					foreach (Resistance r in Creature.Resistances.Values)
+					{
+						if (damage.Types.Contains(r.Type))
+						{
+							if (weakest == null || r.Amount < weakest.Value.Amount)
+							{
+								weakest = r;
+							}
+						}
+					}
+
+					if (weakest != null) // always true
+					{
+						hpLoss -= weakest.Value.Amount;
+					}
+				}
+				#endregion
+
+				#region VULNERABILITY
+				List<DmgType> vulnTypes = new List<DmgType>();
+				foreach (Resistance v in Creature.Vulnerabilities.Values)
+				{
+					vulnTypes.Add(v.Type);
+				}
+
+				bool hasVulnerabilityToAny = false;
+				foreach (DmgType dt in damage.Types)
+				{
+					if (vulnTypes.Contains(dt))
+					{
+						hasVulnerabilityToAny = true;
+						break;
+					}
+				}
+
+				if (hasVulnerabilityToAny)
+				{
+					Resistance? strongest = null;
+					foreach (Resistance v in Creature.Vulnerabilities.Values)
+					{
+						if (damage.Types.Contains(v.Type))
+						{
+							if (strongest == null || v.Amount > strongest.Value.Amount)
+							{
+								strongest = v;
+							}
+						}
+					}
+
+					if (strongest != null) // always true;
+					{
+						hpLoss += strongest.Value.Amount;
+					}
+				}
+				#endregion
+
+				hpLoss = Math.Max(0, hpLoss);
+
+				hpLoss = (int)((float)hpLoss * damageMultiplier);
+
+				TemporaryHitPoints -= hpLoss;
+
+				bool bloodiedAlready = Bloodied;
+				bool dyingAlready = Dying || KO;
+
+				if (TemporaryHitPoints < 0)
+				{
+					hpLoss = -TemporaryHitPoints;
+					HitPoints -= hpLoss;
+					TemporaryHitPoints = 0;
+
+					if (HitPoints <= 0 && ko)
+					{
+						KO = true;
+						HitPoints = 0;
+					}
+				}
+			}
+
+			public void Heal(int amount, Creature healer)
+			{
+				HitPoints = Math.Max(HitPoints, 0);	// move to zero if dying
+				KO = false;	// un-KO
+				HitPoints += amount;
+			}
+		}
+	}
+}
